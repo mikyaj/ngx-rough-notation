9ec49c965cbb0240eeb53836fe4a8615545e5f1f
mudasser <mudasserajaz@gmail.com>
2020-06-10 20:32:33 +0200

    Added option to update styles even after annotation has been drawn

--- projects/ngx-rough-notation/src/lib/components/rough-notation/rough-notation.component.ts
+++ projects/ngx-rough-notation/src/lib/components/rough-notation/rough-notation.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, AfterContentInit, OnDestroy, Input } from '@angular/core';
 import { annotate } from 'rough-notation';
 import { Annotation, types, RoughNotationPadding } from './types';
+
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'rough-notation',
@@ -9,18 +10,43 @@ import { Annotation, types, RoughNotationPadding } from './types';
 
 export class RoughNotationComponent implements AfterContentInit, OnDestroy {
   public annotation: Annotation;
-  private showAnnotation = false;
+  private _show = false;
+  private _color = 'currentColor';
+  private _strokeWidth = 1;
+  private _padding: RoughNotationPadding = 5;
   @Input() type: types;
   @Input() animate = true;
   @Input() animationDuration = 800;
   @Input() animationDelay = 0;
-  @Input() color: string;
-  @Input() strokeWidth = 1;
-  @Input() padding: RoughNotationPadding = 5;
   @Input() iterations = 2;
+
+  @Input()
+  set color(value: string) {
+    this._color = value;
+    if (this.annotation) {
+      this.annotation.color = value;
+    }
+  }
+
+  @Input()
+  set strokeWidth(value: number) {
+    this._strokeWidth = value;
+    if (this.annotation) {
+      this.annotation.strokeWidth = value;
+    }
+  }
+
+  @Input()
+  set padding(value: RoughNotationPadding) {
+    this._padding = value;
+    if (this.annotation) {
+      this.annotation.padding = value;
+    }
+  }
+
   @Input()
   set show(value: boolean) {
-    this.showAnnotation = value;
+    this._show = value;
     if (this.annotation) {
       this.updateAnnotationVisibility(value);
     }
@@ -33,15 +59,15 @@ export class RoughNotationComponent implements AfterContentInit, OnDestroy {
     if (ele && this.type) {
       this.annotation = annotate(ele, {
         type: this.type,
-        color: this.color,
+        color: this._color,
         animate: this.animate,
         animationDuration: this.animationDuration,
         animationDelay: this.animationDelay,
-        strokeWidth: this.strokeWidth,
-        padding: this.padding,
+        strokeWidth: this._strokeWidth,
+        padding: this._padding,
         iterations: this.iterations
       });
-      if (this.showAnnotation === true) {
+      if (this._show === true) {
         this.annotation.show();
       }
     }
